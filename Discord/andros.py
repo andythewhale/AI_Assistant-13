75f3a1547190d738627f05b5b454d6977fe56c7b
Andy Miller <andymiller1313@gmail.com>
2017-07-10 16:58:46 -0500

    Responding bot

--- Discord/andros.py
+++ Discord/andros.py
@@ -1,62 +1,20 @@
-# This is a scratch file for developing the Discord vesion of the bot
-# Andy Miller
-# Jun 9 2017
-
-# Import libraries & grab neccessary objects
-import asyncio
-import json
-import aiohttp
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
 from config import TOKEN
-URL = "https://discordapp.com/api"
 
-# This defines the call to the Discord API.
-# It asks for a response from the URL
-# The response is returned as a JSON.
-async def api_call(path):
-    """
-        Returns the JSON body of a call to the Discord Rest API
-    """
-    
-    with aiohttp.ClientSession() as session:
-        async with session.get(f"{URL}{path}") as response:
-            assert 200 == response.status, response.reason
-            return await response.json()
+Client = discord.Client()
+bot_prefix= "?"
+client = commands.Bot(command_prefix=bot_prefix)
 
-# This program runs the definition based on the gateway path.
-# Notice no TOKEN has been used yet.
-# That is because this main is connecting us to our websocket.
-# The asynchrony of this function is that it continues other processes until this is ready/
-async def main():
-    """Main program."""
-    response = await api_call("/gateway")
-    await start(response["url"])
+@client.event
+async def on_ready():
+    print("Bot Online!")
+    print("Name: {}".format(client.user.name))
+    print("ID: {}".format(client.user.id))
 
-# But where is the start function?
-# Oh it's here:
-# We wait for a message and then we print it
-# But where is the start function?
-# Oh it's here:
-# We wait for a message and then we print it
-async def start(url):
-    with aiohttp.ClientSession() as session:
-        async with session.ws_connect(
-                f"{url}?v=6&encoding=json") as ws:
-            async for msg in ws:
-                data = json.loads(msg.data)
-                
-                if data["op"] == 10: # Hello
-                    await ws.send_json({
-                        "op": 2, #Identity
-                        "d": {
-                            "token": TOKEN,
-                            "properties": {},
-                            "compress": False,
-                            "large_threshold": 250
-                        }
-                    })
-                
-                elif data["op"] == 0: "Dispatch"
-                    print(data['t'], data['d'])
-                    
-                else:
-                    print(data)
+@client.command(pass_context=True)
+async def Bling(ctx):
+    await client.say("Blowe!")
+
+client.run(TOKEN)
